2037fae5ecc4ee667802a51c6be16338e3bfb8f8
Bill Smith <Schmitty2005@gmail.com>
2024-04-15 17:18:30 -0700

    Added midnamer.pas

--- midnamer.pas
+++ midnamer.pas
@@ -10,6 +10,7 @@ uses
 
 function midnamHeader () : String;
 function patchNumber (Number : String; Name : String; ProgramChange : Integer) : String;
+function mc_ControlChange ( Control:Integer; Value:Integer):String;
 
 
 
@@ -29,19 +30,37 @@ END;
 // A function to output the following :
 // -->    "<PatchNameList>"  <--For reference only.  Not included in output!
 //          <Patch Number="A000" Name="Overture K" ProgramChange="0"/>
-function patchNumber (Number : String; Name : String; ProgramChange : Integer) : String;
+function patchNumber(Number:String; Name:String; ProgramChange:Integer):String;
 var
 output1 : String ;
 output2 : String ;
 output3 : String ;
-finalout : String ;
+//finalout : String ;
 begin
   output1 := '<Patch Number=' + AnsiQuotedStr(Number, '"') + ' ';
   output2 := 'Name=' + AnsiQuotedStr(Name,'"') + ' ';
   output3 := 'ProgramChange="'+IntToStr(ProgramChange)+'" />';
-  finalout := output1 + output2 + output3;
-  result := finalout;
+  result := output1 + output2 + output3;
+  //result := finalout;
 end;
+
+
+//<MIDICommands>
+//         <ControlChange Control="32" Value="0"/>
+//</MIDICommands>
+//
+function mc_ControlChange ( Control:Integer; Value:Integer):String;
+var
+header : String = '<MIDICommands>' + sLineBreak;
+footer : String = '</MIDICommands>' + sLineBreak;
+controlc: String = '      <ControlChange Control="';
+begin
+  controlc:= header + controlc + IntToStr(Control) + '"'  +
+             'Value="' + IntToStr(Value) + '"/>' + sLineBreak  + footer;
+  result:= controlc;
+end;
+
+
 
 
 end.
--- midnatester.pas
+++ midnatester.pas
@@ -5,6 +5,7 @@ uses midnamer;
 Begin
 writeln(midnamHeader);
 writeln(patchNumber('01', 'TEST Patch', 00));
+writeln(mc_controlchange(32,0));
 
 End.
 
